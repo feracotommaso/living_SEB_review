fac9289ed11d1918c5146965dc971c89f7423184
tommaso feraco <feracotommaso>
2026-07-02 11:41:30 +0200

    abstracting 06

--- data/1.abstracting_exclude.R
+++ data/1.abstracting_exclude.R
@@ -1,9 +1,75 @@
-#### --------------------------------------------------- 25/01/31 --------------------------------------------------- ####
-d <- readxl::read_excel("data/1.abstracting/25_01_31_deduplicated_processed.xlsx")
+#### --------------------------------------------------- 26/06 --------------------------------------------------- ####
+d <- readxl::read_excel("data/1.abstracting/26_06_deduplicated_processed.xlsx")
 nrow1 <- nrow(d)
 d <- d[d$decision != "exclude", ]
 nrow2 <- nrow(d)
-writexl::write_xlsx(d, "data/2.full_abstracting/25_01_31_fullabstracting.xlsx")
+writexl::write_xlsx(d, "data/2.full_abstracting/26_06_fullabstracting.xlsx")
+nrow1-nrow2 # Excluded
+
+#### --------------------------------------------------- 26/05/01 --------------------------------------------------- ####
+d <- readxl::read_excel("data/1.abstracting/26_06_01_deduplicated_processed.xlsx")
+nrow1 <- nrow(d)
+d <- d[d$decision != "exclude", ]
+nrow2 <- nrow(d)
+writexl::write_xlsx(d, "data/2.full_abstracting/26_06_01_fullabstracting.xlsx")
+nrow1-nrow2 # Excluded
+
+#### --------------------------------------------------- 26/05/01 --------------------------------------------------- ####
+d <- readxl::read_excel("data/1.abstracting/26_05_01_deduplicated_processed.xlsm")
+nrow1 <- nrow(d)
+d <- d[d$decision != "exclude", ]
+nrow2 <- nrow(d)
+writexl::write_xlsx(d, "data/2.full_abstracting/26_05_01_fullabstracting.xlsx")
+nrow1-nrow2 # Excluded
+
+#### --------------------------------------------------- 26/04/01 --------------------------------------------------- ####
+
+#### --------------------------------------------------- 26/03/01 --------------------------------------------------- ####
+d <- readxl::read_excel("data/1.abstracting/26_03_01_deduplicated_processed.xlsx")
+nrow1 <- nrow(d)
+d <- d[d$decision != "exclude", ]
+nrow2 <- nrow(d)
+writexl::write_xlsx(d, "data/2.full_abstracting/26_03_01_fullabstracting.xlsx")
+nrow1-nrow2 # Excluded
+
+#### --------------------------------------------------- 26/02/01 --------------------------------------------------- ####
+d <- readxl::read_excel("data/1.abstracting/26_02_01_deduplicated_processed.xlsx")
+nrow1 <- nrow(d)
+d <- d[d$decision != "exclude", ]
+nrow2 <- nrow(d)
+writexl::write_xlsx(d, "data/2.full_abstracting/26_02_01_fullabstracting.xlsx")
+nrow1-nrow2 # Excluded
+
+#### --------------------------------------------------- 26/01/01 --------------------------------------------------- ####
+d <- readxl::read_excel("data/1.abstracting/26_01_06_deduplicated_processed.xlsx")
+nrow1 <- nrow(d)
+d <- d[d$decision != "exclude", ]
+nrow2 <- nrow(d)
+writexl::write_xlsx(d, "data/2.full_abstracting/26_01_06_fullabstracting.xlsx")
+nrow1-nrow2 # Excluded
+
+#### --------------------------------------------------- 25/12/01 --------------------------------------------------- ####
+d <- readxl::read_excel("data/1.abstracting/25_12_01_deduplicated_processed.xlsx")
+nrow1 <- nrow(d)
+d <- d[d$decision != "exclude", ]
+nrow2 <- nrow(d)
+writexl::write_xlsx(d, "data/2.full_abstracting/25_12_01_fullabstracting.xlsx")
+nrow1-nrow2 # Excluded
+
+#### --------------------------------------------------- 25/11/01 --------------------------------------------------- ####
+d <- readxl::read_excel("data/1.abstracting/25_11_01_deduplicated_processed.xlsx")
+nrow1 <- nrow(d)
+d <- d[d$decision != "exclude", ]
+nrow2 <- nrow(d)
+writexl::write_xlsx(d, "data/2.full_abstracting/25_11_01_fullabstracting.xlsx")
+nrow1-nrow2 # Excluded
+
+#### --------------------------------------------------- 25/09/01 --------------------------------------------------- ####
+d <- readxl::read_excel("data/1.abstracting/25_09_01_deduplicated_processed.xlsx")
+nrow1 <- nrow(d)
+d <- d[d$decision != "exclude", ]
+nrow2 <- nrow(d)
+writexl::write_xlsx(d, "data/2.full_abstracting/25_09_01_fullabstracting.xlsx")
 nrow1-nrow2 # Excluded
 
 #### --------------------------------------------------- 25/04/30 --------------------------------------------------- ####
@@ -13,60 +79,12 @@ d <- d[d$decision != "exclude", ]
 nrow2 <- nrow(d)
 writexl::write_xlsx(d, "data/2.full_abstracting/25_04_30_fullabstracting.xlsx")
 nrow1-nrow2 # Excluded
-#### --------------------------------------------------- 25/09/01 --------------------------------------------------- ####
-d <- readxl::read_excel("data/1.abstracting/25_09_01_deduplicated_processed.xlsx")
+
+#### --------------------------------------------------- 25/01/31 --------------------------------------------------- ####
+d <- readxl::read_excel("data/1.abstracting/25_01_31_deduplicated_processed.xlsx")
 nrow1 <- nrow(d)
 d <- d[d$decision != "exclude", ]
 nrow2 <- nrow(d)
-writexl::write_xlsx(d, "data/2.full_abstracting/25_09_01_fullabstracting.xlsx")
+writexl::write_xlsx(d, "data/2.full_abstracting/25_01_31_fullabstracting.xlsx")
 nrow1-nrow2 # Excluded
-#### --------------------------------------------------- 25/11/01 --------------------------------------------------- ####
-d <- readxl::read_excel("data/1.abstracting/25_11_01_deduplicated_processed.xlsx")
-nrow1 <- nrow(d)
-d <- d[d$decision != "exclude", ]
-nrow2 <- nrow(d)
-writexl::write_xlsx(d, "data/2.full_abstracting/25_11_01_fullabstracting.xlsx")
-nrow1-nrow2 # Excluded
-#### --------------------------------------------------- 25/12/01 --------------------------------------------------- ####
-d <- readxl::read_excel("data/1.abstracting/25_12_01_deduplicated_processed.xlsx")
-nrow1 <- nrow(d)
-d <- d[d$decision != "exclude", ]
-nrow2 <- nrow(d)
-writexl::write_xlsx(d, "data/2.full_abstracting/25_12_01_fullabstracting.xlsx")
-nrow1-nrow2 # Excluded
-#### --------------------------------------------------- 26/01/01 --------------------------------------------------- ####
-d <- readxl::read_excel("data/1.abstracting/26_01_06_deduplicated_processed.xlsx")
-nrow1 <- nrow(d)
-d <- d[d$decision != "exclude", ]
-nrow2 <- nrow(d)
-writexl::write_xlsx(d, "data/2.full_abstracting/26_01_06_fullabstracting.xlsx")
-nrow1-nrow2 # Excluded
-#### --------------------------------------------------- 26/02/01 --------------------------------------------------- ####
-d <- readxl::read_excel("data/1.abstracting/26_02_01_deduplicated_processed.xlsx")
-nrow1 <- nrow(d)
-d <- d[d$decision != "exclude", ]
-nrow2 <- nrow(d)
-writexl::write_xlsx(d, "data/2.full_abstracting/26_02_01_fullabstracting.xlsx")
-nrow1-nrow2 # Excluded
-#### --------------------------------------------------- 26/03/01 --------------------------------------------------- ####
-d <- readxl::read_excel("data/1.abstracting/26_03_01_deduplicated_processed.xlsx")
-nrow1 <- nrow(d)
-d <- d[d$decision != "exclude", ]
-nrow2 <- nrow(d)
-writexl::write_xlsx(d, "data/2.full_abstracting/26_03_01_fullabstracting.xlsx")
-nrow1-nrow2 # Excluded
-#### --------------------------------------------------- 26/04/01 --------------------------------------------------- ####
-#### --------------------------------------------------- 26/05/01 --------------------------------------------------- ####
-d <- readxl::read_excel("data/1.abstracting/26_05_01_deduplicated_processed.xlsm")
-nrow1 <- nrow(d)
-d <- d[d$decision != "exclude", ]
-nrow2 <- nrow(d)
-writexl::write_xlsx(d, "data/2.full_abstracting/26_05_01_fullabstracting.xlsx")
-nrow1-nrow2 # Excluded
-#### --------------------------------------------------- 26/05/01 --------------------------------------------------- ####
-d <- readxl::read_excel("data/1.abstracting/26_06_01_deduplicated_processed.xlsx")
-nrow1 <- nrow(d)
-d <- d[d$decision != "exclude", ]
-nrow2 <- nrow(d)
-writexl::write_xlsx(d, "data/2.full_abstracting/26_06_01_fullabstracting.xlsx")
-nrow1-nrow2 # Excluded
+
